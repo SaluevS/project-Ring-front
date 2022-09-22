906d0c6015d820879d9550b631f12ecb29c0b309
Saluev <ibragim.saluev@mail.ru>
2022-09-22 16:43:46 +0300

    Стили

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,6 @@ import SignIn from './components/Auth/SignIn';
 import SignUp from './components/Auth/SignUp';
 import { Routes, Route } from 'react-router-dom'
 import Discussions from './components/Discussions/Discussions';
-import Header from './components/Header/Header';
-import ThemeMap from './components/ThemeMap/ThemeMap';
 import ThemeOne from './components/ThemeOne/ThemeOne';
 import Home from './components/Home/Home';
 
@@ -12,7 +10,6 @@ import Home from './components/Home/Home';
 const App = () => {
   return (
     <>
-    <Header />
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/auth' element={<SignUp />} />
--- src/components/ThemeOne/ThemeOne.jsx
+++ src/components/ThemeOne/ThemeOne.jsx
@@ -1,83 +1,121 @@
-import React from 'react';
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
-import { fetchThemes } from '../../features/themeSlice';
-import { addComment, addLike, delLike, fetchComments } from '../../features/commentSlice';
-import styles from '../ThemeOne/themeOne.module.css';
-import { useState } from 'react';
+import React from "react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { fetchThemes } from "../../features/themeSlice";
+import {
+  addComment,
+  addLike,
+  delLike,
+  fetchComments,
+} from "../../features/commentSlice";
+import styles from "../ThemeOne/themeOne.module.css";
+import { useState } from "react";
+import Header2 from "../Header2/Header2";
 
 const ThemeOne = () => {
-    const [comm, setComm] = useState('')
-    const [like, setLike] = useState(true)
-    const { id } = useParams();
-    const themes = useSelector((state) => state.themeSlice.themes)
-    const comments = useSelector((state) => state.commentSlice.comments)
-    const userId = useSelector((state) => state.applicationSlice.login1)
-    const dispatch = useDispatch();
+  const [comm, setComm] = useState("");
+  const [like, setLike] = useState(true);
+  const { id } = useParams();
+  const themes = useSelector((state) => state.themeSlice.themes);
+  const comments = useSelector((state) => state.commentSlice.comments);
+  const userId = useSelector((state) => state.applicationSlice.login1);
+  const dispatch = useDispatch();
 
-    const filteredThemes = themes.filter((elem) => {
-        if (!id) return true
+  const filteredThemes = themes.filter((elem) => {
+    if (!id) return true;
 
-        return id === elem._id
-    })
+    return id === elem._id;
+  });
 
-    const filteredComms = comments.filter((item) => {
-        console.log(item.theme._id);
-        if (!id) return true
+  const filteredComms = comments.filter((item) => {
+    console.log(item.theme._id);
+    if (!id) return true;
 
-        return id === item.theme._id
-    })
+    return id === item.theme._id;
+  });
 
-    const handleChange = (e) => {
-        setComm(e.target.value)
+  const handleChange = (e) => {
+    setComm(e.target.value);
+  };
+
+  useEffect(() => {
+    dispatch(fetchThemes());
+    dispatch(fetchComments());
+  }, [dispatch]);
+
+  const handleAddComment = () => {
+    dispatch(addComment({ comm, id, userId }));
+  };
+
+  const handleAddLike = (userId, commId) => {
+    if (like) {
+      dispatch(addLike({ userId, commId }));
+      setLike(false);
     }
-
-    useEffect(() => {
-        dispatch(fetchThemes())
-        dispatch(fetchComments())
-    }, [dispatch])
-
-    const handleAddComment = () => {
-        dispatch(addComment({ comm, id, userId }))
+    if (!like) {
+      dispatch(delLike({ userId, commId }));
+      setLike(true);
     }
+  };
 
-    const handleAddLike = ( userId, commId) => {
-        if (like) {
-            dispatch(addLike({ userId, commId }))
-            setLike(false)
-        }
-        if (!like) {
-            dispatch(delLike({ userId, commId }))
-            setLike(true)
-        }
-    }
+  return (
 
-    return (
-        <div>
-            {filteredThemes.map((elem) => {
-                return (
-                    <div>{elem.name}</div>
-                )
-            })}
-            {filteredComms.map((item) => {
-                return (
-                    <div>
-                        {item.user.login}
-                        {item.text}
-                        <div>
-                            <button className={styles.buttonLike} onClick={() => handleAddLike(userId, item._id)}>❤️</button>
-                            <div>{item.like.length}</div>
-                        </div>
-                    </div>
-                )
-            })}
-            <div className={styles.commMain}>
-                <input className={styles.inputComm} value={comm} onChange={handleChange} />
-                <button className={styles.buttonComm} onClick={handleAddComment}>Добавить</button>
+    <>
+    <Header2/>
+    <div className={styles.w}>
+    <div className={styles.One}>
+      {filteredThemes.map((elem) => {
+        return (
+          <div className={styles.theme}>
+            <div className={styles.blocks}>
+              <div className={styles.title}>{elem.name}</div>
+              <div className={styles.text}>{elem.text}</div>
             </div>
-        </div>
-    );
+            <div className={styles.blocks}>
+              <img
+                src="https://gamerwall.pro/uploads/posts/2022-02/1645706299_28-gamerwall-pro-p-volshebnik-gendalf-krasivie-oboi-40.jpg"
+                alt="q"
+              />
+            </div>
+          </div>
+        );
+      })}
+      <div className={styles.commMain}>
+        <input
+          className={styles.inputComm}
+          value={comm}
+          placeholder='Написать комментарий...'
+          onChange={handleChange}
+        />
+        <button className={styles.buttonComm} onClick={handleAddComment}>
+          Добавить
+        </button>
+      </div>
+      {filteredComms
+        .map((item) => {
+          return (
+            <div className={styles.commit}>
+              <div>
+                <h3>{item.user.login}</h3>
+              </div>
+              <div className={styles.coms}>
+                <div className={styles.textCo}>{item.text}</div>
+                <div className={styles.cos}><button
+                  className={styles.buttonLike}
+                  onClick={() => handleAddLike(userId, item._id)}
+                >
+                  ❤️
+                </button>{item.like.length}</div>
+                </div>
+            </div>
+          );
+        })
+        .reverse()}
+    </div>
+    </div>
+    </>
+  );
 };
 
 export default ThemeOne;
