3152e9a58f170e0105cc19751bedfad09455a4b1
Saluev <ibragim.saluev@mail.ru>
2022-09-22 10:00:04 +0300

    Исправил

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import SignIn from './components/Auth/SignIn';
 import SignUp from './components/Auth/SignUp';
 import { Routes, Route } from 'react-router-dom'
 import Discussions from './components/Discussions/Discussions';
-import Theme from './components/Theme/Theme';
+import Header from './components/Header/Header';
 import ThemeMap from './components/ThemeMap/ThemeMap';
 import ThemeOne from './components/ThemeOne/ThemeOne';
 import Home from './components/Home/Home';
@@ -11,6 +11,7 @@ import Home from './components/Home/Home';
 const App = () => {
   return (
     <>
+    <Header />
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/auth' element={<SignUp />} />
